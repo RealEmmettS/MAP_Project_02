b38713830fa6b8ec365f0c273386467e9923eb4e
Emmett Shaughnessy <github@emmetts.dev>
2021-08-19 09:46:43 -0500

    init

---
+++ MAP - Project 02/MAP - Project 02/Extra Code.swift
@@ -0,0 +1,9 @@
+//
+//  Extra Code.swift
+//  MAP - Project 02
+//
+//  Created by Emmett Shaughnessy on 8/19/21.
+//
+
+import Foundation
+
---
+++ MAP - Project 02/MAP - Project 02/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MAP - Project 02
+//
+//  Created by Emmett Shaughnessy on 8/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+    }
+    
+    
+    
+    
+
+
+}
+
